db7ca764640562653e44db5fdef980aca1d3ca26
ranjan kumar <ranjanbhiya2@gmail.com>
2023-03-28 23:51:03 +0530

    static css file added

--- notesapp/views.py
+++ notesapp/views.py
@@ -1,9 +1,12 @@
 from datetime import date
-from django.utils import timezone
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .models import KeepNote
+
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .models import KeepNote
+
 
 def BasePage(request):
     return render(request, 'base.html')
@@ -30,7 +33,7 @@ def AddNewNotes(request):
         add.notes = request.POST.get('note')
         add.created_on = note_created_on
         add.save()
-        return HttpResponseRedirect('../home')
+        return redirect('/home')
 
     return render(request, 'newnotes.html')
 
@@ -40,18 +43,14 @@ def UpdateNote(request, id):
         update.title = request.POST.get('title')
         update.notes = request.POST.get('note')
         update.save()
-        return HttpResponseRedirect('../home')
+        return redirect('/home')
     return render(request, "editnote.html", {"update":update})
 
 def DeleteNote(request, id):
     noteDelete = KeepNote.objects.get(id=id)
-    print(noteDelete, "\n\n\n\n")
     try:
-        print("DDDDDDDDD \n\n\n\n\n")
         if noteDelete:
-            print("DDDDDDDDD \n\n\n\n\n")
             noteDelete.delete()
-            print("PRPRPRPPRPRPR \n\n\n\n")
-            return HttpResponseRedirect('../home')
+            return redirect('/home')
     except:
         raise ValueError(f"{id} Dosen't exist.")
--- notesproject/settings.py
+++ notesproject/settings.py
@@ -1,6 +1,6 @@
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'notesapp',
 ]
 
@@ -110,14 +111,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = f"{BASE_DIR}/static"
+STATIC_URL = '/static/'
+# STATIC_ROOT = f"{BASE_DIR}/static"
 
-STATICFILE_DIR = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATICFILES_DIRS = [
-#     BASE_DIR / 'mystaticfiles'
+#     BASE_DIR,'static'
 # ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
